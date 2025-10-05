de9b17fcf050adebfe453c4b2847dfdf8c75ecd7
nh <wq1880@naver.com>
2025-10-05 20:15:39 +0000

    Train_model_AandB

---
+++ dataset/bbh/bbh_all_data/count.py
@@ -0,0 +1,9 @@
+import json
+import random
+import os
+
+input_path = "all_task_train_right_answer.json"
+with open(input_path, "r", encoding='utf-8') as f:
+    data = json.load(f)
+
+print(len(data))
---
+++ dataset/bbh/bbh_all_data/split_data.py
@@ -0,0 +1,36 @@
+import json
+import random
+import os
+
+def split_data(input_path, f_A, f_B, split_ratio=0.5, seed=42):
+    with open(input_path, "r", encoding='utf-8') as f:
+        data = json.load(f)
+
+    total = len(data)
+    random.seed(seed)
+    random.shuffle(data)
+
+    split_idx = int(total * split_ratio)
+    data_A = data[:split_idx]
+    data_B = data[split_idx:]
+
+    with open(f_A, "w", encoding="utf-8") as f:
+        json.dump(data_A, f, ensure_ascii=False, indent=4)
+    with open(f_B, "w", encoding="utf-8") as f:
+        json.dump(data_B, f, ensure_ascii=False, indent=4)
+
+    #why the fuck can't type korean ssibal geotgatnea
+    print(f"✅ Split complete!")
+    print(f" ├─ Total samples : {total}")
+    print(f" ├─ A set : {len(data_A)} samples → {os.path.abspath(f_A)}")
+    print(f" └─ B set : {len(data_A)} samples → {os.path.abspath(f_B)}")
+
+input_path = "all_task_train_right_wronghint_answer.json"
+f_A = "all_task_train_right_wronghint_answer_C.json"
+f_B = "all_task_train_right_wronghint_answer_D.json"
+
+split_data(
+    input_path = input_path,
+    f_A = f_A,
+    f_B = f_B
+)
